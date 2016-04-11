e91a645dfc1943091c355b2290ca8af21d4a8898
Giga <Giga.Chkhikvadze@gmail.com>
2016-04-11 23:27:13 +0400

    Update mail.js

--- server/config/mail.js
+++ server/config/mail.js
@@ -1,19 +1,19 @@
 
 module.exports.mailgun = {
 	//Your api key, from Mailgun’s Control Panel
-	apiKey: 'key-3b4f538196b2d3c9a69723e03d45ab27',
+	apiKey: 'key-3b5f538196b2d3c9a69723e03d45ab27',
 	//Your domain, from the Mailgun Control Panel
-	domain: 'sandbox6a9664debf1b4bc6a15d95822344a930.mailgun.org',
+	domain: '',
 	//Your sending email address
-	support: 'support@lingwing.com',
-	noreply: 'noreply@lingwing.com',
-	invoice: 'invoice@lingwing.com',
-	defaultFrom: 'Invoice ',
+	support: 'support@gigachkhikvadze.com',
+	noreply: 'noreply@gigachkhikvadze.com',
+	invoice: 'invoice@gigachkhikvadze.com',
+	defaultFrom: 'Create ',
 	getnoreply: function (email) {
 		return (email || this.defaultFrom) + " <" + this.noreply + ">";
 	},
 	getsupport: function () {
-		return "lingwing.com Team <" + this.support + ">";
+		return "gigachkhikvadze.com  <" + this.support + ">";
 	},
 	getinvoice: function (email) {
 		return (email || this.defaultFrom) + " <" + this.invoice + ">";
